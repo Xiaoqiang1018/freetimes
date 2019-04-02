f1b5e89a15bbfeea4cf0d03b48c9251fdfa5c515
xiaoqiang <yulongfox@gmail.com>
2019-04-02 23:31:08 +0800

    springboot

--- client-backend/src/test/java/com/tcm/clientbackend/ClientBackendApplicationTests.java
+++ client-backend/src/test/java/com/tcm/clientbackend/ClientBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.house.houseservice;
+package com.tcm.clientbackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HouseServiceApplicationTests {
+public class ClientBackendApplicationTests {
 
     @Test
     public void contextLoads() {
